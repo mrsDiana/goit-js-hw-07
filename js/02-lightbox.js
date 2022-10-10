afb2666bbeafd4671430035e78c9a8cc5a2ad870
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-10-10 18:08:42 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryItemsGrid = galleryItems
+  .map(
+    (item) => `
+<a class="gallery__item" href="${item.original}">
+<img class="gallery__image" 
+src="${item.preview}" 
+alt="${item.description}">
+</a> `
+  )
+  .join("");
+  const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", galleryItemsGrid);
+
 console.log(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: '250'});
